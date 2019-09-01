1907d9c77de7e4414dfa28bb18dfbd0ef41d1b0f
Generoso Martello <generoso.martello@gmail.com>
2019-09-01 19:53:52 +0200

    minor bug fix

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -32,12 +32,16 @@ self.__precacheManifest = [
     "revision": "dc08d157dc079d7e5ad51e7b4e0aa8b5"
   },
   {
+    "url": "app/content/docs/examples/alice/ch_3.html",
+    "revision": "cfd716cd8caf9f443744d1b68a0043eb"
+  },
+  {
     "url": "app/content/docs/examples/links.css",
-    "revision": "300f68df4ae2415d93a52a566b256dbf"
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
   },
   {
     "url": "app/content/docs/examples/links.html",
-    "revision": "cbd247603f5f438b7c760f4fdc77cf50"
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
   },
   {
     "url": "app/templates/mdl_card.css",
@@ -61,7 +65,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "ae237c2132295ab9c703b839fe18f652"
+    "revision": "26001245c2d3582093d89da3c73aacf0"
   },
   {
     "url": "index.js",
@@ -158,6 +162,38 @@ self.__precacheManifest = [
   {
     "url": "app/content/docs/examples/alice/img/ch_3_2.png",
     "revision": "6f21a396f2c8b7f3a5a884dc9a8932c0"
+  },
+  {
+    "url": "app/content/docs/examples/images/avatar_01.png",
+    "revision": "8b86e5f8249828163a0d7e9f6d315dce"
+  },
+  {
+    "url": "app/content/docs/examples/images/avatar_02.png",
+    "revision": "8a011d6e36c3b968241ac4331cd03aad"
+  },
+  {
+    "url": "app/content/docs/examples/images/avatar_03.png",
+    "revision": "57f804786860e3d1166d0905651efed2"
+  },
+  {
+    "url": "app/content/docs/examples/images/card_cover_2.jpg",
+    "revision": "7988b42acee615124d83c43f3f34c02b"
+  },
+  {
+    "url": "app/content/docs/examples/images/card_cover.jpg",
+    "revision": "056b0eb64ac30441bd6aca8e3798286b"
+  },
+  {
+    "url": "app/content/docs/examples/images/card_placeholder.jpg",
+    "revision": "67f0870f50d4008e45eca50cd43e50bf"
+  },
+  {
+    "url": "app/content/docs/examples/images/cover_javascript.jpg",
+    "revision": "86f9d6c2b7e0bf1703681ada474a313d"
+  },
+  {
+    "url": "app/content/docs/examples/images/cover_recipes.jpg",
+    "revision": "6b19a1781bc4e0ca2d71e843ec7ad38e"
   },
   {
     "url": "images/api.png",
