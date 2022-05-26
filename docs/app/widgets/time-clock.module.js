9870430551965decb3fd5313fe05b80bdee99d49
Gene <genemars@users.noreply.github.com>
2022-05-26 18:55:59 +0200

    Fixes absolute widget URL in time-clock.module.js

--- docs/app/widgets/time-clock.module.js
+++ docs/app/widgets/time-clock.module.js
@@ -2,7 +2,7 @@ import 'https://cdn.jsdelivr.net/npm/zuix-dist@1.1.6/js/zuix.module.min.js';
 
 customElements.define('time-clock', class extends HTMLElement {
   connectedCallback() {
-    zuix.loadComponent(this, 'widgets/time-clock', '', {
+    zuix.loadComponent(this, 'https://zuixjs.org/app/widgets/time-clock', '', {
       container: this.attachShadow({mode: 'closed'})
     });
   }
