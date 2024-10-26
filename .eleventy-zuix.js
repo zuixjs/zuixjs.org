cd2eb0327467a77cc2beebaa568ff513cd43a479
gene <generoso.martello@gmail.com>
2024-10-26 14:11:41 +0200

    Updated font size.

--- .eleventy-zuix.js
+++ .eleventy-zuix.js
@@ -557,7 +557,7 @@ function configure(eleventyConfig) {
   });
 
   eleventyConfig.addShortcode('tryLink', function(text, link) {
-    return `<div layout="column center-left" style="margin-left: 16px"><div><a layout="row center-start" href="${link}">
+    return `<div layout="column center-left" style="padding-left: 16px"><div><a layout="row center-start" href="${link}">
          <i class="material-icons mdl-color-text--primary">try</i>
          <span style="font-size: 120%;margin-left:6px;margin-bottom: 2px">${text}</span>
        </a></div></div>`;
