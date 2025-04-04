3456abd71b00a475eb7c02d356ea0f00871d39c7
gene <generoso.martello@gmail.com>
2025-04-04 22:40:44 +0200

    Disabled service worker.

--- docs/config.js
+++ docs/config.js
@@ -20,11 +20,4 @@
     "cookiesConsent": true,
     "siteMapUrl": "https://zuixjs.org/"
   });
-  // Check that service workers are registered
-  if ('serviceWorker' in navigator) {
-    // Use the window load event to keep the page load performant
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js');
-    });
-  }
 })();
